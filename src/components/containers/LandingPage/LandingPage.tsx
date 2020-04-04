2554510996ef9cf06d93ba6547a695f7ecc9d134
fongelias <fongelias@gmail.com>
2020-04-04 19:36:11 -0400

    react router setup and landing page component

---
+++ src/components/containers/LandingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FlexBox } from 'components/presentational/FlexBox/FlexBox';
+
+import './LandingPage.scss';
+
+export const LandingPage = () => (
+    <FlexBox direction="column">
+        <h1>learn</h1>
+        <h1>contribute</h1>
+    </FlexBox>
+)
